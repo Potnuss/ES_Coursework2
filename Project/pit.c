48017d772b1946f5f47a00ff80ac825ae97e1904
Pontus Petersson <potnuss@gmail.com>
2015-03-07 20:05:26 +0000

    Refined pit.c

--- Project/pit.c
+++ Project/pit.c
@@ -4,17 +4,19 @@
 
 void pit_init(int time)
 {
-	// enable PIT interrupt in Nested Vectored Interrupt Controller 
+	SIM_SCGC6 |= SIM_SCGC6_PIT_MASK; //PIT clock gate control
+
+	PIT_MCR &= ~PIT_MCR_MDIS_MASK; //enable the module clock. This bit must be enabled before any other setup is done.
+
+	
+	//Enable PIT interrupt in Nested Vectored Interrupt Controller 
 	NVICICPR2 |= 1 << (68 % 32);
 	NVICISER2 |= 1 << (68 % 32);
 	NVICIP68 |= 3;
 
-	SIM_SCGC6 |= SIM_SCGC6_PIT_MASK;
+	PIT_LDVAL0 = time; //Timer Start Value BitsThese bits set the timer start value
 
-	PIT_MCR = 0;
+	PIT_TCTRL0 |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK; //enables the timer | Interrupt will be requested whenever TIF is set.
 
-	PIT_LDVAL0 = time;
-
-	PIT_TCTRL0 |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
-	
+	//No need for Timer Flag Register (PIT_TFLGn)?
 }
